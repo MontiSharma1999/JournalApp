0efd321216a8c1cc016a1948e4837c21bb69f79d
MontiSharma1999 <113963712+MontiSharma1999@users.noreply.github.com>
2025-01-12 13:44:46 +0530

    something went wrong

--- src/test/java/ez/codebits/journalApp/JournalAppApplicationTests.java
+++ src/test/java/ez/codebits/journalApp/JournalAppApplicationTests.java
@@ -2,14 +2,14 @@ package ez.codebits.journalApp;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class JournalAppApplicationTests {
 
-	@Disabled
+
 	@Test
+	@Disabled
 	void contextLoads() {
+		// TODO document why this method is empty
 	}
 
 }
